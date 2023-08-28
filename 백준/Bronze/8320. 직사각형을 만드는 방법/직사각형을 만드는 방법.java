3223cbec89c224012ec31d57435316f07790fd95
HeeHiHee <111411965+HeeHiHee@users.noreply.github.com>
2023-08-29 08:59:58 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 144 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); // 정사각형 개수
+		
+		int cnt=0;
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=i; j<=n; j++) {
+				if(i*j<=n) {
+					cnt++;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+	
+	}
+
+}
