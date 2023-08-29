95ef7a47ffa6d5c8ee755b184f4f40a886b0587e
HeeHiHee <111411965+HeeHiHee@users.noreply.github.com>
2023-08-29 09:03:44 +0900

    [Bronze V] Title: 새싹, Time: 76 ms, Memory: 11208 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		String[] st1 = {" ", " ", " ", " ", " ", " ", " ", " ", " ", ",","r","\'", "\"", "7"};
+		for(int i=0; i<st1.length; i++) {
+			System.out.print(st1[i]);
+		}
+		System.out.println();
+		String[] st2 = {"r", "`", "-", "_", " ", " ", " ", ",","\'", " ", " ",",","/"," "};
+		for(int i=0; i<st2.length; i++) {
+			System.out.print(st2[i]);
+		}
+		System.out.println();
+		String[] st3 = {" ", "\\", ".", " ","\"","."," ","L","_","r","\'"};
+		for(int i=0; i<st3.length; i++) {
+			System.out.print(st3[i]);
+		}
+		System.out.println();
+		String[] st4 = {" "," "," ","`","~","\\","/"};
+		for(int i=0; i<st4.length; i++) {
+			System.out.print(st4[i]);
+		}
+		System.out.println();
+		String[] st5 = {" "," "," "," "," "," ","|"};
+		for(int i=0; i<st5.length; i++) {
+			System.out.print(st5[i]);
+		}
+		System.out.println();
+		String[] st6 = {" "," "," "," "," "," ","|"};
+		for(int i=0; i<st6.length; i++) {
+			System.out.print(st6[i]);
+		}
+	}
+
+}
