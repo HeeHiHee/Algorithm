23efe8377023924084374d85bf4dc6e368467775
HeeHiHee <111411965+HeeHiHee@users.noreply.github.com>
2023-08-29 10:12:02 +0900

    [Silver IV] Title: 수 찾기, Time: 2600 ms, Memory: 189488 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,69 +5,52 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
-		int[] arr = new int[N];
+		int N  = sc.nextInt();
+		int[] arrN = new int[N];
 		for(int i=0; i<N; i++) {
-			arr[i] = sc.nextInt();
+			arrN[i] = sc.nextInt();
 		}
 		
-		// 이진탐색을 위해 배열 정렬하기
-		Arrays.sort(arr);
+		Arrays.sort(arrN); // 오름차순으로 정렬 
 		
 		int M = sc.nextInt();
-		
-		int lo = 0;
-		int hi = arr[arr.length-1];
-		
-		for(int i=0; i<M; i++) {
-			int tmp = sc.nextInt();
+		for(int m=0; m<M; m++) {
+			int x = sc.nextInt();
 			
-			// 값이 있으면 1, 없으면 0 출력
-			if(binarySearch(arr, tmp) >= 0) {
-				System.out.println(1);
-			}else {
-				System.out.println(0);
-			}
-		}// M번 반복 끝
+			int lo = 0;
+			int hi = N-1; // 5
+			int mid;
+			
+			int res = 0; // 존재하지 않을 때를 초기 값으로 잡음 
+			
+			while(lo <= hi) {
+				
+				mid = (lo + hi) / 2;
+				
+				// x가 mid보다 왼쪽에 있을 때는 hi를 mid로 내리기
+				// x가 mid보다 오른쪽에 있을 때는 lo를 mid로 올리기 
+				if(x < arrN[mid]) {
+					hi = mid-1;
+				}
+				else if(x > arrN[mid]) {
+					lo = mid+1;
+				}else {
+					res = 1;
+					break;
+				}				
+				
+				
+			}//while
+			
+			System.out.println(res);
+			
+			
+			
+			
+		}// M
 		
 		
-	}
-	
-
-	public static int binarySearch(int[] arr, int key) {
-		int lo = 0;
-		int hi = arr.length - 1;
-
-		// lo가 hi보다 커지기 전까지 반복
-		while (lo <= hi) {
-			/*
-			 * 1. 중간 인덱스를 구한다. 2. 중간 값과 key값을 비교한다. 3. 비교 값에 따라 범위를 왼쪽, 오른쪽 혹은 값이 같은 경우는 해당
-			 * 인덱스를 반환한다.
-			 */
-
-			int mid = (lo + hi) / 2;
-
-			// 중간 값 보다 작을 때
-			if (key < arr[mid]) {
-				hi = mid - 1;
-
-			}
-			// 중간 값 보다 클 때
-			else if (key > arr[mid]) {
-				lo = mid + 1;
-
-			}
-			// 중간 값과 같을 때
-			else {
-				return mid;
-
-			}
-		}
-
-		// while안에서 반환 값이 없다는 말은 찾고자 하는 값이 없다는 것임.
-		// 찾고자 하는 값이 존재하지 않을 경우
-		return -1;
-
-	}
+		
+	}//main
 
 }
