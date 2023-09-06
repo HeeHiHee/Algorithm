79fb348b3548635b74ac7131ac755d965bf29894
HeeHiHee <111411965+HeeHiHee@users.noreply.github.com>
2023-09-06 15:29:53 +0900

    [Silver III] Title: N과 M (3), Time: 660 ms, Memory: 315720 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static int N;
+	static int M;
+	static int[] nums;
+	static boolean[] visit;
+	
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		nums = new int[M]; // 출력할 배열, 크기는 M
+		visit = new boolean[N+1]; // 자연수 사용 여부를 체크할 배열
+		
+		sequance(0);
+		
+		System.out.println(sb);
+		
+		
+	}//main
+	
+	public static void sequance(int idx) {
+		// 기저파트
+		if(idx==M) {
+			for(int i=0; i<M; i++) {
+				sb.append(nums[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		// 재귀파트
+		// i : 자연수 사용여부 배열을 돎
+		// idx : 출력할 배열의 index
+		for(int i=1; i <= N; i++) {
+			if(!visit[i]) {
+				nums[idx] = i; // idx번째 배열에 i를 저장 
+				
+//				visit[i] = true; // 방문했음을 표시 
+				sequance(idx+1); // 다음 배열로 넘어감
+				
+//				visit[i] = false; // 방문하고 돌아오면 다시 초기화 
+			}
+		}
+		
+		
+		
+	}// sequance
+	
+	
+}
