4cd45b5035bda4b24a6d0ab8c8174d6178f0e5c5
따잇 <111411965+HeeHiHee@users.noreply.github.com>
2024-02-08 01:10:45 +0900

    [Bronze V] Title: A+B - 8, Time: 256 ms, Memory: 18932 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        for(int t=1; t<=T; t++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int sum = A+B;
+            sb.append("Case #" + t + ": "+ A + " + " + B + " = " + sum).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
