466361e72426b022c15c86cfd4018f98aaa99cc7
따잇 <111411965+HeeHiHee@users.noreply.github.com>
2024-08-26 11:59:28 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 16024 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,57 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int M;
-	static int[] nums;
-	static boolean[] visit;
-	
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		nums = new int[M]; // 출력할 배열, 크기는 M
-		visit = new boolean[N+1]; // 자연수 사용 여부를 체크할 배열
-		
-		sequance(0,1);
-		
-		System.out.println(sb);
-		
-		
-	}//main
-	
-	public static void sequance(int idx, int start) {
-		// 기저파트
-		if(idx==M) {
-			for(int i=0; i<M; i++) {
-				sb.append(nums[i] + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		// 재귀파트
-		// i : 자연수 사용여부 배열을 돎
-		// idx : 출력할 배열의 index
-		for(int i=start; i <= N; i++) {
-			if(!visit[i]) {
-				nums[idx] = i; // idx번째 배열에 i를 저장 
-				
-//				visit[i] = true; // 방문했음을 표시 
-				sequance(idx+1, i); // 다음 배열로 넘어감
-				
-//				visit[i] = false; // 방문하고 돌아오면 다시 초기화 
-			}
-		}
-		
-		
-		
-	}// sequance
-	
-	
+    static int N,M;
+    static int[] ans;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        ans = new int[M];
+        DFS(0,0);
+        System.out.print(sb);
+    }
+
+    public static void DFS(int depth, int start){
+        if(depth == M){
+            for(int x : ans){
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start; i<N; i++){
+            ans[depth] = i+1;
+            DFS(depth+1, i);
+        }
+    }
+
+
 }
