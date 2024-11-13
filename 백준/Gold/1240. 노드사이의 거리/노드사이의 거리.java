701b19da4b50699d90ebf1781d8b77fca69f2511
HeeHiHee <dahee121816@gmail.com>
2024-11-13 14:44:29 +0900

    [Gold V] Title: 노드사이의 거리, Time: 332 ms, Memory: 36616 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -1,54 +1,45 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static int N, M, tree[][], u, v, d, start, end;
+    static int N, M, u, v, d, start, end;
+    static ArrayList<ArrayList<int[]>> list = new ArrayList<>();
     static boolean visit[];
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        tree = new int[N+1][N+1];
+        for(int i=0; i<N+1; i++){list.add(new ArrayList<>());}
         visit = new boolean[N+1];
         for(int i=0; i<N-1; i++){
             u = sc.nextInt();
             v = sc.nextInt();
             d = sc.nextInt();
-            tree[u][v] = d;
-            tree[v][u] = d;
+            list.get(u).add(new int[]{v, d});
+            list.get(v).add(new int[]{u, d});
         }
         for(int i=0; i<M; i++){
             start = sc.nextInt();
             end = sc.nextInt();
-            BFS(start);
+            DFS(start, 0);
             visit = new boolean[N+1];
         }
         System.out.println(sb);
 
     }
 
-    public static void BFS(int start){
-        Queue<int[]> q = new LinkedList<>();
+    public static void DFS(int start, int d){
         visit[start] = true;
-        q.add(new int[]{start, 0});
-
-        while(!q.isEmpty()){
-            int[] tmp = q.poll();
-            int n = tmp[0]; // 노드 번호
-            int d = tmp[1]; // 지금까지 이동한 거리
-            // 현재 노드와 도착점이 연결되어 있다면
-            if(tree[n][end]!=0){
-                sb.append(d+tree[n][end]).append("\n");
-                break;
-            }
-            // 도착점이랑 연결되어있지 않다면
-            for(int i=1; i<N+1; i++){
-                if(tree[n][i]!=0 && !visit[i]){
-                    visit[i] = true;
-                    q.add(new int[]{i, d+tree[n][i]});
-                }
+        // 도착 노드와 간선이 있다면
+        if(start==end){
+            sb.append(d).append("\n");
+            return;
+        }
+        // 연결된 간선이 없다면
+        for(int[] n : list.get(start)){
+            if(!visit[n[0]]){
+                DFS(n[0], d+n[1]);
             }
         }
     }
