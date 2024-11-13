91facb310fdd1a044b2d316ab7b4dc3345c49635
HeeHiHee <dahee121816@gmail.com>
2024-11-13 17:34:41 +0900

    [Gold IV] Title: 트리의 지름, Time: 400 ms, Memory: 44300 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,39 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    static int N, u, v, d, ans=0;
-    static boolean[] visit, child; // child:자식이 있으면(true)
+    static int N, u, v, d, ans=0, node1=0;
+    static boolean[] visit;
     static ArrayList<ArrayList<int[]>> list = new ArrayList<>();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
         visit = new boolean[N+1];
-//        child = new boolean[N+1];
         for(int i=0; i<N+1; i++){list.add(new ArrayList<>());}
         for(int i=0; i<N-1; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            u = Integer.parseInt(st.nextToken());
-            v = Integer.parseInt(st.nextToken());
-            d = Integer.parseInt(st.nextToken());
+            u = sc.nextInt();
+            v = sc.nextInt();
+            d = sc.nextInt();
             list.get(u).add(new int[]{v,d});
             list.get(v).add(new int[]{u,d});
-//            child[u] = true; // 노드 u는 자식이 있는 노드
         }
-
-        for(int i=1; i<N+1; i++){
-            DFS(i, 0);
-            for(int j=0; j<N+1; j++){visit[j]=false;}
-        }
+        DFS(1, 0);
+        for(int j=0; j<N+1; j++){visit[j]=false;}
+        DFS(node1, 0);
         System.out.println(ans);
     }
 
     public static void DFS(int node, int sum){
         visit[node] = true;
-
         for(int[] a : list.get(node)){
             int n = a[0]; // 노드 번호
             int d = a[1]; // 가중치
@@ -41,8 +32,6 @@ public class Main {
                 DFS(n, sum+d);
             }
         }
-
-        ans = Math.max(ans, sum);
-
+        if(ans<sum){ans = sum; node1 = node;}
     }
 }
