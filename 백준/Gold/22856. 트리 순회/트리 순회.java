f22e5cda669dee0c7a52763a55857ed75783e99e
HeeHiHee <dahee121816@gmail.com>
2024-11-15 18:41:30 +0900

    [Gold IV] Title: 트리 순회, Time: 848 ms, Memory: 228208 KB -BaekjoonHub

--- 백준/Gold/22856. 트리 순회/트리 순회.java
+++ 백준/Gold/22856. 트리 순회/트리 순회.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static int N, a, b, c, cnt=0, v=0, res=0;
+    static int N, a, b, c, res=0, level=0;
     static boolean[] visit;
     static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
     public static void main(String[] args) {
@@ -14,35 +14,14 @@ public class Main {
             a=sc.nextInt(); b=sc.nextInt(); c=sc.nextInt();
             list.get(a).add(b);
             list.get(a).add(c);
+            if(b!=-1) res+=2;
+            if(c!=-1) res+=2;
         }
-        Inorder(1);
-        System.out.println(res);
-
+        Left(1);
+        System.out.println(res-level);
     }
-
-    public static void Inorder(int node){
-        int left = list.get(node).get(0);
-        int right = list.get(node).get(1);
-
-        if(left==-1 && right==-1){
-            visit[node] = true;
-            v++;
-            if(v==N){res = cnt;}
-            return;
-        }
-
-        if(left!=-1 &&!visit[left]){
-            cnt++;
-            Inorder(left);
-            cnt++;
-        }
-        visit[node] = true;
-        v++;
-        if(v==N){res = cnt;} // 노드에 자식이 2개 아닌 경우
-        if(right!=-1 && !visit[right]){
-            cnt++;
-            Inorder(right);
-            cnt++;
-        }
+    public static void Left(int node){
+        int left = list.get(node).get(1);
+        if(left!=-1){level++; Left(left);}
     }
 }
