c7f7d521d2aa349c7b4bd402931f9d6cb89a7131
HeeHiHee <dahee121816@gmail.com>
2024-11-18 15:58:41 +0900

    [Gold II] Title: 트리의 지름, Time: 1284 ms, Memory: 322604 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -20,24 +20,19 @@ public class Main {
                 list.get(n).add(new int[]{v,s});
             }
         }
-        visit[1] = true;
         DFS(1,0);
         visit = new boolean[N];
-        visit[node1] = true;
         DFS(node1, 0);
         System.out.println(d);
     }
 
     public static void DFS(int node, int sum){
-
-        for(int i=0; i<list.get(node).size(); i++){
-            int n = list.get(node).get(i)[0];
-            int s = list.get(node).get(i)[1];
-            if(!visit[n]){
-                visit[n] = true;
-                DFS(n, sum+s);
+        if(d<sum){d=sum; node1=node;}
+        visit[node] = true;
+        for(int[] a :list.get(node)){
+            if(!visit[a[0]]){
+                DFS(a[0], sum+a[1]);
             }
         }
-        if(d<sum){d=sum; node1=node;}
     }
 }
