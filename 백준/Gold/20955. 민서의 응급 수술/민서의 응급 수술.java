36c2d593bc484e436f5f6a18727748fd8200dea1
HeeHiHee <dahee121816@gmail.com>
2024-11-19 16:11:15 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 628 ms, Memory: 61060 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.java
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.java
@@ -3,54 +3,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    static int N, M, edge;
+    static int N, M, edge; // edge:사이클 제거하기 위한 간선 수
     static boolean[] visit;
     static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // 노드와 간선 입력받기
-        N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken())+1;
         M = Integer.parseInt(st.nextToken());
-        visit = new boolean[N + 1];
-        for (int i = 0; i <= N; i++) list.add(new ArrayList<>());
-
-        // 간선 정보 입력받기
-        for (int i = 0; i < M; i++) {
+        visit = new boolean[N];
+        for(int i=0; i<N; i++){list.add(new ArrayList<>());}
+        for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             list.get(u).add(v);
-            list.get(v).add(u); // 무방향 그래프
+            list.get(v).add(u);
         }
 
-        int compnt = 0; // 연결 컴포넌트 개수
-
-        // 모든 노드를 탐색하며 컴포넌트 개수와 사이클 간선을 계산
-        for (int i = 1; i <= N; i++) {
-            if (!visit[i]) {
+        int compnt = 0; // 컴포넌트 개수
+        for(int i=1; i<N; i++){
+            if(!visit[i]){
                 compnt++;
-                DFS1(i, -1); // -1은 부모 노드가 없음을 의미
+                DFS1(i, -1);
             }
         }
-
-        // 사이클 간선은 DFS에서 중복 계산되므로 절반으로 나누기
-        System.out.println(edge / 2 + (compnt - 1));
+        System.out.println(edge / 2 + (compnt-1));
     }
 
-    public static void DFS1(int node, int parent) {
+    public static void DFS1(int node, int parent){
         visit[node] = true;
-        for (int n : list.get(node)) {
-            if (!visit[n]) {
+        for(int n : list.get(node)){
+            if (!visit[n]){
                 DFS1(n, node);
-            } else if (n != parent) {
-                // 방문했지만 부모 노드가 아닌 경우, 사이클 간선 발견
+            }else if(n != parent){
                 edge++;
             }
         }
     }
+
 }
