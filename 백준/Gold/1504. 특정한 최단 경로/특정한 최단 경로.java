17517f7a2bf992c37bd538f8736a248e51364fb0
HeeHiHee <dahee121816@gmail.com>
2024-11-20 16:19:40 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 480 ms, Memory: 67504 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -26,39 +26,18 @@ public class Main {
         int v2 = Integer.parseInt(st.nextToken());
         //case1 : 1-v1-v2-N 순으로 방문
         //case2 : 1-v2-v1-N 순으로 방문
-        boolean case1 = true; // 경로가 있는지 확인
-        boolean case2 = true; // 경로가 있는지 확인
-        Dhstr(1);
-        int oneTov1 = cost[v1]; if(oneTov1 == Integer.MAX_VALUE) case1=false;
-        int oneTov2 = cost[v2]; if(oneTov2 == Integer.MAX_VALUE) case2=false;
-
-        // 방문배열, 비용배열 초기화
-        for(int i=0; i<N; i++){cost[i]=Integer.MAX_VALUE; visit[i]=0;}
-        Dhstr(v1);
-        int v1Tov2 = cost[v2]; if(v1Tov2 == Integer.MAX_VALUE) case1=false;
-        int v1ToN = cost[N-1]; if(v1ToN == Integer.MAX_VALUE) case2=false;
-
-        // 방문배열, 비용배열 초기화
-        for(int i=0; i<N; i++){cost[i]=Integer.MAX_VALUE; visit[i]=0;}
-        Dhstr(v2);
-        int v2ToN = cost[N-1]; if(v2ToN == Integer.MAX_VALUE) case1=false;
-        int v2Tov1 = cost[v1]; if(v2Tov1 == Integer.MAX_VALUE) case2=false;
-
-        int ans = -1;
-        if(case1 && case2){
-            ans = Math.min(oneTov1+v1Tov2+v2ToN, oneTov2+v1ToN+v2Tov1);
-        }else if(case1){
-            ans = oneTov1+v1Tov2+v2ToN;
-        }else if(case2){
-            ans = oneTov2+v1ToN+v2Tov1;
-        }
-        System.out.println(ans);
-
+        int case1 = -1, case2 = -1;
+        int a1 = Dhstr(1,v1), a2 = Dhstr(v1,v2), a3 = Dhstr(v2,N-1),
+                b1 = Dhstr(1,v2), b3 = Dhstr(v1,N-1);
+        if(a1>=0 && a2>=0 && a3>=0) case1 = a1+a2+a3;
+        if(b1>=0 && a2>=0 && b3>=0) case2 = b1+a2+b3;
+        System.out.println(case1!=-1&&case2!=-1?Math.min(case1, case2):Math.max(case1, case2));
     }
 
-    public static void Dhstr(int start){
+    public static int Dhstr(int start, int end){
+        // 방문배열, 비용배열 초기화
+        for(int i=0; i<N; i++){cost[i]=Integer.MAX_VALUE; visit[i]=0;}
         cost[start] = 0;
-
         while (true){
             int next = -1; // 다음에 방문할 노드 번호
             int d = Integer.MAX_VALUE; // 최소비용
@@ -74,6 +53,6 @@ public class Main {
                 }
             }
         }
-
+        return cost[end]==Integer.MAX_VALUE?-1:cost[end];
     }
 }
