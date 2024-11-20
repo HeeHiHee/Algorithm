431be310d51b52d96dfe2c9a327d76059946b936
HeeHiHee <dahee121816@gmail.com>
2024-11-20 14:54:21 +0900

    [Gold V] Title: 최소비용 구하기, Time: 328 ms, Memory: 45976 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,78 +2,49 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int start, end;
-	static int[][] map;
-	static long[] arr;
-	static boolean[] visited;
+    static int N, M, cost[], start, end, visit[]; // cost : 도시까지 가는데 드는 최소 비용을 저장할 배열
+    static ArrayList<ArrayList<int[]>> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine())+1;
+        M = Integer.parseInt(br.readLine());
+        cost = new int[N]; visit = new int[N];
+        for(int i=0; i<N; i++){cost[i]=Integer.MAX_VALUE;} // 최대 값으로 배열 초기화
+        for(int i=0; i<N; i++){list.add(new ArrayList<>());}
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list.get(u).add(new int[]{v,c});
+        }
+        st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken()); end = Integer.parseInt(st.nextToken());
+        Dhstr(start);
+        System.out.println(cost[end]);
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void Dhstr(int s){
+        cost[s] = 0; // 시작 노드는 거리 0으로 초기화
 
-		N = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-
-		map = new int[N + 1][N + 1];
-
-		// 초기화
-		// 비용이 0보다 크거나 같으므로
-		for (int i = 0; i < N + 1; i++) {
-			for (int j = 0; j < N + 1; j++) {
-				map[i][j] = Integer.MAX_VALUE;
-			}
-		}
-
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int go = Integer.parseInt(st.nextToken()); // 출발 번호
-			int stop = Integer.parseInt(st.nextToken()); // 도착 번호
-			int pay = Integer.parseInt(st.nextToken()); // 비용
-			
-			// 같은 경로이면 비용이 더 적은걸로
-			map[go][stop] = Math.min(map[go][stop], pay);
-
-		} // 입력 끝
-
-		st = new StringTokenizer(br.readLine());
-		start = Integer.parseInt(st.nextToken());
-		end = Integer.parseInt(st.nextToken());
-
-		arr = new long[N + 1]; // 최소 비용을 저장할 배열
-		Arrays.fill(arr, Integer.MAX_VALUE); // 배열을 최댓값으로 채우기
-		arr[start] = 0; // 출발점은 0으로
-		visited = new boolean[N + 1];
-		int cnt = 0;
-
-		while (cnt < N) {
-			int min = Integer.MAX_VALUE;
-			int idx = 0;
-			for (int i = 0; i < N + 1; i++) {
-				if (!visited[i] && (arr[i] < min)) {
-					min = (int) arr[i];
-					idx = i;
-				}
-			}
-			
-			visited[idx] = true;
-			for (int i = 0; i < N + 1; i++) {
-				if (!visited[i] && map[idx][i] != Integer.MAX_VALUE && (arr[idx] + map[idx][i]) < arr[i]) {
-					arr[i] = arr[idx] + map[idx][i];
-				}
-			}
-
-			cnt++;
-
-		}
-
-		System.out.println(arr[end]);
-
-	}// main
-
+        while(true){
+            int next = -1; // 가장 비용이 적은 노드(다음 방문할 노드)
+            int d = Integer.MAX_VALUE; // 비용
+            for(int i=1; i<N; i++){
+                if(visit[i]==0 && cost[i]<d){next=i; d=cost[i];} // 다음 방문할 노드 찾기
+            }
+            if(next==-1) break; // 더이상 방문할 노드가 없으면 탈출
+            visit[next] = 1; // 방문처리
+            for(int[] i : list.get(next)){
+                // (현재 도시(next)의 비용 + 다음 도시 비용)이 저장된 다음 도시 최소비용보다 적으면 갱신
+                if(cost[next]+i[1]<cost[i[0]]){
+                    cost[i[0]] = cost[next]+i[1];
+                }
+            }
+        }
+    }
 }
